d9dd9f74ef7283666ff53563b6daa2bfd2ef9976
Jay-45-byte <jraut6518@gmail.com>
2025-01-27 20:52:10 +0530

    Add animation for About Section

--- scripts/animation.js
+++ scripts/animation.js
@@ -61,3 +61,75 @@ gsap.from("#right-div-s1 img:nth-child(2)", {
   duration: 2,
   delay: 3,
 });
+
+const t2 = gsap.timeline({
+  scrollTrigger: {
+    // markers: true,
+    trigger: "#name2",
+    scroller: "body",
+    start: "top 40%",
+  },
+})
+
+t2.from("#profile-img",{
+  y: -100,
+  duration: 0.5,
+  stagger:0.3,
+  opacity: 0,
+  ease: "bounce.out",
+})
+
+t2.from("#profile-img img",{
+  x: -100,
+  rotation:360,
+  opacity: 0,
+  duration: 1.5,
+  ease: "elastic.out(1.5,0.4)",
+})
+
+t2.from("#name2", {
+  x: 70,
+  opacity: 0,
+  duration: 0.3,
+});
+
+t2.from("#profile-name2", {
+  x: -40,
+  opacity: 0,
+  duration: 0.3,
+});
+
+t2.from("#links img",{
+  y: -100,
+  opacity: 0,
+  stagger:0.3,
+  duration:0.3,
+  ease: "bounce.out",
+})
+
+t2.from("#heading p", {
+  y: 40,
+  opacity: 0,
+  duration: 0.2,
+});
+
+t2.from("#heading img",{
+  rotate:360,
+  opacity:0,
+  duration:1,
+})
+
+t2.from("#about > p", {
+  y: -100,
+  opacity: 0,
+  duration: 0.5,
+  stagger:0.3,
+  ease: "bounce.out",
+});
+
+t2.from("#para1 img",{
+  rotate:-30,
+  duration:1,
+  repeat: -1,
+  yoyo: true,
+})
